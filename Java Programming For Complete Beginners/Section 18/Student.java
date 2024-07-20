20b80236f7f95d54a53f5793da5dc3f7b44667a3
spurohit434 <shailesh.purohit@watchguard.com>
2024-07-20 13:05:08 +0530

    Add files via upload

---
+++ Java Programming For Complete Beginners/Section 18/Student.java
@@ -0,0 +1,75 @@
+package com.intro_arrays;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Student {
+	private String name;
+	private ArrayList<Integer> marks = new ArrayList<Integer>();
+
+	public Student(String name, int[] marks) {
+		this.name = name;
+		for (int mark : marks) {
+			this.marks.add(mark);
+		}
+	}
+
+	int getNumberOfMarks() {
+		return marks.size(); // in arrayList we use ,size() whereas in array we use .length
+	}
+
+	int getTotalSumOfMarks() {
+		int sum = 0;
+		for (int mark : marks) {
+			sum += mark;
+		}
+		return sum;
+	}
+
+	int getMaximumMarks() {
+//		int sum = 0;
+//		for (int mark : marks) {
+//			if (sum < mark) {
+//				sum = mark;
+//			}
+//		}
+//		return sum;
+		return Collections.max(marks);
+	}
+
+	int getMinimumMarks() {
+//		int sum = Integer.MAX_VALUE;
+//		for (int mark : marks) {
+//			if (sum > mark) {
+//				sum = mark;
+//			}
+//		}
+//		return sum;
+		return Collections.min(marks);
+	}
+
+	BigDecimal getAverageMarks() {
+		int sum = 0;
+		for (int mark : marks) {
+			sum += mark;
+		}
+		sum = sum / marks.size();
+		BigDecimal bg = new BigDecimal(sum);
+		return bg;
+	}
+
+	public void addMark(int mark) {
+		marks.add(mark);
+	}
+
+	public void removeMarksAtIndex(int index) {
+		marks.remove(index);
+	}
+
+	@Override
+	public String toString() { // used to return the name of the student and marks array
+		return name + marks;
+	}
+
+}
---
+++ Java Programming For Complete Beginners/Section 18/StudentRunner.java
@@ -0,0 +1,31 @@
+package com.intro_arrays;
+
+import java.math.BigDecimal;
+
+public class StudentRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] marks = { 99, 98, 100, 87 };
+		Student s1 = new Student("Arun", marks);
+		int number = s1.getNumberOfMarks();
+		int sum = s1.getTotalSumOfMarks();
+		int max = s1.getMaximumMarks();
+		int min = s1.getMinimumMarks();
+		BigDecimal avg = s1.getAverageMarks();
+
+		System.out.println(number);
+		System.out.println(sum);
+		System.out.println(max);
+		System.out.println(min);
+		System.out.println(avg);
+
+		System.out.println(s1);
+
+		s1.addMark(35);
+		s1.removeMarksAtIndex(3);
+
+		System.out.println(s1);
+	}
+
+}
